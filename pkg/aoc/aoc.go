142ae4d91246dedb7e7c85887c5341bb3404112d
Simon Hylander <simon.hylander@byggdagboken.se>
2022-11-30 15:12:11 +0100

    puzzle map

--- pkg/aoc/aoc.go
+++ pkg/aoc/aoc.go
@@ -13,6 +13,30 @@ type Puzzle interface {
 
 var puzzles = map[int]Puzzle{
 	1: days.Day1{},
+	2: days.Day2{},
+	3: days.Day3{},
+	4: days.Day4{},
+	5: days.Day5{},
+	// 6:  days.Day6{},
+	// 7:  days.Day7{},
+	// 8:  days.Day8{},
+	// 9:  days.Day9{},
+	// 10: days.Day10{},
+	// 11: days.Day11{},
+	// 12: days.Day12{},
+	// 13: days.Day13{},
+	// 14: days.Day14{},
+	// 15: days.Day15{},
+	// 16: days.Day16{},
+	// 17: days.Day17{},
+	// 18: days.Day18{},
+	// 19: days.Day19{},
+	// 20: days.Day20{},
+	// 21: days.Day21{},
+	// 22: days.Day22{},
+	// 23: days.Day23{},
+	// 24: days.Day24{},
+	// 25: days.Day25{},
 }
 
 func NewPuzzle(day int) Puzzle {
